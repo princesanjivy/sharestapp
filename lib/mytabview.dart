184183773937ad4b095ca1fd4f8ffa397b5487fe
PrinceSanjivy <sanjivy.android@gmail.com>
2020-11-04 21:35:27 +0530

    removed sidemenu

--- lib/mytabview.dart
+++ lib/mytabview.dart
@@ -1,6 +1,5 @@
 import 'package:sharestapp/font_awesome_icons.dart';
 import 'package:sharestapp/homepage.dart';
-import 'package:sharestapp/sidemenu.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sharestapp/wastatuspage.dart';
